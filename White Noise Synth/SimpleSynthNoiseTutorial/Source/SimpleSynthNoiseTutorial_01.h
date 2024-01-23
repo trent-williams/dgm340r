3543efc7bac67448df940a00a620e61b3feec6e3
trent-williams <10848261@uvu.edu>
2024-01-23 10:18:06 -0700

    Update to use Decibel scale on sliders

--- White Noise Synth/SimpleSynthNoiseTutorial/Source/SimpleSynthNoiseTutorial_01.h
+++ White Noise Synth/SimpleSynthNoiseTutorial/Source/SimpleSynthNoiseTutorial_01.h
@@ -48,6 +48,28 @@
 #pragma once
 
 //==============================================================================
+class DecibelSlider : public juce::Slider
+{
+public :
+    DecibelSlider() {}
+
+    double getValueFromText(const juce::String& text) override
+    {
+        auto minusInfinitydB = -100.0;
+        auto decibelText = text.upToFirstOccurrenceOf("dB", false, false).trim();
+
+        return decibelText.equalsIgnoreCase("-INF") ? minusInfinitydB : decibelText.getDoubleValue();
+    }
+    
+    juce::String getTextFromValue(double value) override
+    {
+        return juce::Decibels::toString(value);
+    }
+
+private :
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DecibelSlider)
+};
+
 class MainContentComponent   : public juce::AudioAppComponent
 {
 public:
@@ -56,13 +78,33 @@ public:
         setSize (800, 600);
         setAudioChannels (0, 2); // no inputs, two outputs
 
-        //GUI
+        /* //Old GUI
         levelSliderLeft.setRange(0, 0.25);
         levelSliderLeft.setTextBoxStyle(juce::Slider::TextBoxRight, true, 100, 25);
         levelSliderRight.setRange(0, 0.25);
         levelSliderRight.setTextBoxStyle(juce::Slider::TextBoxRight, true, 100, 25);
         levelLabelLeft.setText("Noise Level Left", juce::dontSendNotification);
         levelLabelRight.setText("Noise Level Right", juce::dontSendNotification);
+        */
+
+        // Note to self. Always check slider ranges. Ranges for linear sliders are
+        // incredibly small compared to sliders scaled to decibels.
+
+        // New GUI in dB, hypothetically
+        levelSliderLeft.setRange(-100, -12);
+        levelSliderLeft.setTextBoxStyle(juce::Slider::TextBoxRight, true, 70, 25);
+        levelSliderLeft.onValueChange = [this] { levelLeftIndB = juce::Decibels::decibelsToGain((float)levelSliderLeft.getValue()); };
+        levelSliderLeft.setValue(juce::Decibels::gainToDecibels(levelLeftIndB));
+        
+
+        levelSliderRight.setRange(-100, -12);
+        levelSliderRight.setTextBoxStyle(juce::Slider::TextBoxRight, true, 70, 25);
+        levelSliderRight.onValueChange = [this] { levelRightIndB = juce::Decibels::decibelsToGain((float)levelSliderRight.getValue()); };
+        levelSliderRight.setValue(juce::Decibels::gainToDecibels(levelRightIndB));
+        
+
+        levelLabelLeft.setText("Noise Level Left in dB", juce::dontSendNotification);
+        levelLabelRight.setText("Noise Level Right in dB", juce::dontSendNotification);
 
         // Make things visable
         addAndMakeVisible(levelSliderLeft);
@@ -92,10 +134,10 @@ public:
 
     void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override
     {
-        auto levelLeft = (float)levelSliderLeft.getValue();
-        auto levelRight = (float)levelSliderRight.getValue();
-        auto levelScaleLeft = levelLeft * 2.0f;
-        auto levelScaleRight = levelRight * 2.0f;
+        auto currentLevelLeft = levelLeftIndB; // (float)levelSliderLeft.getValue();
+        auto currentLevelRight = levelRightIndB; // (float)levelSliderRight.getValue();
+        auto levelScaleLeft = currentLevelLeft * 2.0f;
+        auto levelScaleRight = currentLevelRight * 2.0f;
 
         for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
         {
@@ -106,12 +148,12 @@ public:
             if (channel == 0)
             {
                 for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
-                    buffer[sample] = random.nextFloat() * levelScaleLeft - levelLeft;
+                    buffer[sample] = random.nextFloat() * levelScaleLeft - currentLevelLeft;
             }
             else
             {
                 for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
-                    buffer[sample] = random.nextFloat() * levelScaleRight - levelRight;
+                    buffer[sample] = random.nextFloat() * levelScaleRight - currentLevelRight;
             }
             
         }
@@ -119,19 +161,24 @@ public:
 
     void resized() override
     {
-        levelLabelLeft.setBounds(10, 10, 90, 20);
-        levelLabelRight.setBounds(10, 30, 90, 20);
-        levelSliderLeft.setBounds(100, 10, getWidth() - 110, 20);
-        levelSliderRight.setBounds(100, 30, getWidth() -110, 20);
+        levelLabelLeft.setBounds(10, 10, 140, 20);
+        levelLabelRight.setBounds(10, 30, 140, 20);
+        levelSliderLeft.setBounds(150, 10, getWidth() - 160, 20);
+        levelSliderRight.setBounds(150, 30, getWidth() -160, 20);
     }
 
 private:
     juce::Random random;
 
-    juce::Slider levelSliderLeft;
-    juce::Slider levelSliderRight;
+    //juce::Slider levelSliderLeft;
+    //juce::Slider levelSliderRight;
+    DecibelSlider levelSliderLeft;
+    DecibelSlider levelSliderRight;
     juce::Label levelLabelLeft;
     juce::Label levelLabelRight;
+
+    float levelLeftIndB = 0.0f;
+    float levelRightIndB = 0.0f;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
